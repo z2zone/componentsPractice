a4a6695fa12cf436f372916b5746d2476cde4b0e
Brandon Hyungjun Cho <hjun1114@gmail.com>
2023-02-05 22:47:45 -0500

    adding basic dropdown visibility logic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Dropdown from './Components/Dropdown';
 
 const App = () => {
     const options = [
-        {label: "Heesun", value: "m"},
-        {label: "Heesun", value: "m"},
-        {label: "Heesun", value: "m"},
-        {label: "Heesun", value: "m"},
-        {label: "Heesun", value: "m"},
-        {label: "Heesun", value: "m"}
+        {label: "hee", value: "m1"},
+        {label: "mon", value: "m2"},
+        {label: "min", value: "m3"},
+        {label: "nic", value: "m4"},
+        {label: "jung", value: "m5"},
+        {label: "mar", value: "m6"}
     ];
     return(
         <Dropdown options={options}/>
--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,7 +1,32 @@
+import {useState} from 'react';
+
 const Dropdown = ({options}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOptionClick = (selectedOption) =>{
+        console.log(selectedOption);
+        setIsOpen(false);
+    }
+
+    const handleToggleOpen = () =>{
+        setIsOpen(!isOpen);
+    }
+
+    const renderedList = options.map(option=>{
+        return(
+            <div 
+                key={option.value}
+                onClick={()=>{handleOptionClick(option)}}
+            >
+                {option.label}
+            </div>
+        );
+    });
+
     return (
         <div>
-            Dropdown
+            <div onClick={handleToggleOpen}>Select...</div>
+            {isOpen && renderedList}
         </div>
     );
 }
