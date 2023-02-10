b0fa845e808c9dc8b3c193882225618948afe1e7
Brandon Cho <hjun1114@gmail.com>
2023-02-09 22:26:33 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import Dropdown from './Components/Dropdown';
-import {useState} from 'react';
-
 const App = () => {
 
-    const [selected, setSelection] = useState(null);
-    const handleSelection = (option) => {
-        setSelection(option);
-    }
-    const options = [
-        {label: "hee", value: "m1"},
-        {label: "mon", value: "m2"},
-        {label: "min", value: "m3"},
-        {label: "nic", value: "m4"},
-        {label: "jung", value: "m5"},
-        {label: "mar", value: "m6"}
-    ];
     return(
-        <Dropdown options={options} onChange={handleSelection} value={selected}/>
+        <div></div>
     );
 }
 
---
+++ src/Pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import Dropdown from '../Components/Dropdown';
+import {useState} from 'react';
+
+const DropdownPage = () => {
+
+    const [selected, setSelection] = useState(null);
+    const handleSelection = (option) => {
+        setSelection(option);
+    }
+    const options = [
+        {label: "hee", value: "m1"},
+        {label: "mon", value: "m2"},
+        {label: "min", value: "m3"},
+        {label: "nic", value: "m4"},
+        {label: "jung", value: "m5"},
+        {label: "mar", value: "m6"}
+    ];
+    return(
+        <Dropdown options={options} onChange={handleSelection} value={selected}/>
+    );
+}
+
+export default DropdownPage;
