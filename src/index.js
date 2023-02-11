158e52c350fb4fde01efdf19a1f7fc17bcb35362
Brandon Cho <hjun1114@gmail.com>
2023-02-10 23:52:32 -0500

    adding context to navigation component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { NavigationProvider } from './Context/Navigation';
 
 const rootElement = document.getElementById('root');
 const reactRoot = ReactDOM.createRoot(rootElement);
 
-reactRoot.render(<App/>);
+reactRoot.render(
+    <NavigationProvider>
+        <App/>
+    </NavigationProvider>  
+);
