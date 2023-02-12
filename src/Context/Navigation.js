84a788f00084193e8556976c4633b39105493e81
Brandon Cho <hjun1114@gmail.com>
2023-02-11 23:55:02 -0500

    adding eventListener for backward forward path

--- src/Context/Navigation.js
+++ src/Context/Navigation.js
@@ -1,10 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({children}) => {
+
+    const [windowPath, setWindowPath] = useState(window.location.pathname);
+    useEffect = (()=>{
+        const handler = () =>{
+            setWindowPath(window.location.pathname);
+        }
+        window.addEventListener('popstate', handler);
+    
+        return () => {
+            window.removeEventListener('popstate', handler);
+        }
+    },[]);
+    
     return (
         <NavigationContext.Provider value={{}}>
+            {windowPath}
             {children}
         </NavigationContext.Provider>  
     );
