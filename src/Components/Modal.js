ff54024cd50c186d3ae4a0bae6454f9c7a8cd0d0
Brandon Cho <hjun1114@gmail.com>
2023-02-14 23:47:09 -0500

    adding portal and logic for modal

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,6 +1,12 @@
-const Modal = () => {
-    return (
-        <div>Modal</div>
+import ReactDOM from 'react-dom';
+
+const Modal = ({handleClose}) => {
+    return ReactDOM.createPortal(
+        <div>
+            <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={handleClose}></div>
+            <div className="absolute inset-40 p-10 bg-white">modal component</div>
+        </div>,
+        document.querySelector(".modal-container")
     );
 }
 
--- src/Pages/ModalPage.js
+++ src/Pages/ModalPage.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Modal from '../Components/Modal';
 
 const ModalPage = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const handleModalClick = () => {
+        setModalOpen(true);
+    }
+    const handleClose = () => {
+        setModalOpen(false);
+    }
     return (
         <div>
-            <Modal/>
+            <button onClick={handleModalClick}>Open Modal</button>
+            {modalOpen && <Modal handleClose={handleClose}/>}
         </div>
     );  
 }
