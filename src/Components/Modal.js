3fe0cda0c169b65eb13c50d4695dedc68e19dd55
Brandon Cho <hjun1114@gmail.com>
2023-02-15 22:20:11 -0500

    adding style to modal and fix scrolling bug

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,10 +1,27 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
-const Modal = ({handleClose}) => {
+const Modal = ({handleClose, children, acceptButton}) => {
+    
+    useState(()=>{
+        //grab the body element and add a class so that scrolling cannot happen.
+        document.body.classList.add("overflow-hidden");
+        return ()=>{
+            document.body.classList.remove("overflow-hidden");
+        }
+    },[]);
+
+    const renderedButton = <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleClose}>Accept</button>;
+
     return ReactDOM.createPortal(
         <div>
-            <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={handleClose}></div>
-            <div className="absolute inset-40 p-10 bg-white">modal component</div>
+            <div className="fixed inset-0 bg-gray-300 opacity-80" onClick={handleClose}></div>
+            <div className="fixed inset-40 p-10 bg-white">
+                <div className='flex flex-col justify-between h-full'>
+                    {children}
+                    <div className='flex justify-end'>{acceptButton && renderedButton}</div>
+                </div>
+            </div>
         </div>,
         document.querySelector(".modal-container")
     );
--- src/Pages/ModalPage.js
+++ src/Pages/ModalPage.js
@@ -11,8 +11,20 @@ const ModalPage = () => {
     }
     return (
         <div>
-            <button onClick={handleModalClick}>Open Modal</button>
-            {modalOpen && <Modal handleClose={handleClose}/>}
+            <button 
+                onClick={handleModalClick}
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                >
+                Open Modal
+            </button>
+            { modalOpen && 
+                <Modal 
+                    handleClose={handleClose}
+                    acceptButton={true}
+                >
+                    "Metacognition is, put simply, thinking about one's thinking. More precisely, it refers to the processes used to plan, monitor, and assess one's understanding and performance. Metacognition includes a critical awareness of a) one's thinking and learning and b) oneself as a thinker and learner."
+                </Modal>
+            }
         </div>
     );  
 }
