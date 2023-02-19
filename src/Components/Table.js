a1f16599bbcc5c4dde67f0c3292fc19c97529019
Brandon Cho <hjun1114@gmail.com>
2023-02-18 23:50:19 -0500

    refactoring table to make it more reusable

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,13 +1,25 @@
-const Table = ({data}) => {
+const Table = ({data, dataConfig}) => {
+
+    const renderThead = dataConfig.map(obj => {
+        return <th key={obj.label}>{obj.label}</th>;
+    });
+
+    const renderTbody = data.map((rowData)=>{
+        const renderTrow = dataConfig.map((columnData)=>{
+            return <td key={columnData.label} className="p-3">{columnData.render(rowData)}</td>;
+        });
+
+        return <tr key={rowData.name} className="border-b">{renderTrow}</tr>
+    });
 
     return(
         <table>
             <thead>
-                <tr>Company</tr>
-                <tr>Color</tr>
-                <tr>Score</tr>
+                {renderThead}
             </thead>
-            <tbody></tbody>
+            <tbody>
+                {renderTbody}
+            </tbody>
         </table>
     );
 }
--- src/Pages/TablePage.js
+++ src/Pages/TablePage.js
@@ -1,17 +1,34 @@
 import Table from "../Components/Table";
 
 const TablePage = () => {
+
     const data = [
         {name:"Apple", color:"bg-red-500", score:100},
         {name:"Samsung", color:"bg-blue-500", score:80},
         {name:"Microsoft", color:"bg-orange-500", score:95},
         {name:"IBM", color:"bg-blue-500", score:70},
-        {name:"Bloomberg", color:"bg-black-500", score:90},
+        {name:"Bloomberg", color:"bg-black", score:90},
         {name:"Salesforce", color:"bg-blue-500", score:85}
     ];
+
+    const dataConfig = [
+        {
+            label: "Company", 
+            render: (obj) => obj.name
+        },
+        {
+            label: "Color", 
+            render: (obj) => <div className={`p-3 m-2 ${obj.color}`}></div>
+        },
+        {
+            label: "Score", 
+            render: (obj) => obj.score
+        }
+    ];
+    
     return (
         <div>
-            <Table data={data}/>
+            <Table data={data} dataConfig={dataConfig}/>
         </div>
     );
 }
