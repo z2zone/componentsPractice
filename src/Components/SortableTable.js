b07b230258b5ab70555e87c8bf9c4ee8527db40a
Brandon Cho <hjun1114@gmail.com>
2023-02-22 23:57:50 -0500

    adding click event and state for header sort

--- src/Components/SortableTable.js
+++ src/Components/SortableTable.js
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import Table from './Table';
 
 const SortableTable = (props) => {
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
     const { dataConfig } = props;
+    
+    const handleClick = (label) =>{
+        if(sortOrder === null){
+            setSortOrder('asc');
+            setSortBy(label);
+        }else if(sortOrder === 'asc'){
+            setSortOrder('desc');
+            setSortBy(label);
+        }else if(sortOrder ==='desc'){
+            setSortOrder(null);
+            setSortBy(null);
+        }
+    }
 
     const updatedDataConfig = dataConfig.map((columnObject)=>{
         if(columnObject.sortValue){
             return {
                 ...columnObject,
-                header: () => <th>{columnObject.label} sortable</th>
+                header: () => <th onClick={()=>{handleClick(columnObject.label)}}>{columnObject.label} sortable</th>
             };
         }else{
-            return;
+            return columnObject;
         }
     });
 
